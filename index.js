ae2a133af8ed8afc81d17a807297a393a9a2a58d
mhmtaltnts <12mart1973@gmail.com>
2021-12-10 19:35:57 +0300

    audio added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const navBar = document.getElementById("navBar")
 const main = document.getElementById("main")
 const slider = document.getElementById("slider")
+const playBtn = document.getElementById("play-btn")
+const stopBtn = document.getElementById("stop-btn")
+const pauseBtn = document.getElementById("pause-btn")
+const sound = new Audio("/sounds/bells.mp3")
+const bell = document.getElementById("bell")
 
 slider.addEventListener("click" , changeTheme)
 console.log(navBar.className)
@@ -12,6 +17,27 @@ function changeTheme(){
         navBar.classList.remove("dark")
         main.classList.remove("dark")
     }
+}
+/*playBtn.onClick =()=>{
+    sound.play()
+}
+*/
 
+playBtn.addEventListener("click", playAudio)
+pauseBtn.addEventListener("click", pauseAudio)
+stopBtn.addEventListener("click", stopAudio)
 
+function playAudio(){
+    sound.play()
+    bell.classList.add("animate")
+}
+function pauseAudio(){
+    sound.pause()
+    bell.classList.remove("animate")
+}
+
+function stopAudio(){
+    sound.pause()
+    sound.currentTime = 0
+    bell.classList.remove("animate")
 }
